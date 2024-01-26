98b80ae6af93da18079aadd86ab9690b038ce796
Chris Riley <20461606+cjriley20@users.noreply.github.com>
2024-01-25 20:53:39 -0500

    Fix exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+  for (const value of values) {
+    let index = array.indexOf(value, 0);
+    while (index != -1) {
+      array.splice(index, 1);
+      index = array.indexOf(value, index);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
