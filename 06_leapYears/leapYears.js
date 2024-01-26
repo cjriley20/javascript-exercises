1042699bb81e9ea3989215a8892bc46303aed3c4
Chris Riley <20461606+cjriley20@users.noreply.github.com>
2024-01-26 18:21:16 -0500

    Fix exercise 6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    divisibleBy4 = year % 4 == 0;
+    divisibleBy100 = year % 100 == 0;
+    divisibleBy400 = year % 400 == 0;
 
+    return divisibleBy400 || (divisibleBy4 && !divisibleBy100);
 };
 
 // Do not edit below this line
